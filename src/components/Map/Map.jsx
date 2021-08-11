3eb35db68735fa6a58808c975043aa4390fd7b6b
unknown <matt.mcquain@gmail.com>
2021-08-10 20:19:03 -0700

    map and place name displaying correctly

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
+
+const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width: 600px)'); //isMobile is set to false if the device is larger than 600px
+
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyD1JRnhN60frPHB96jrs5amDEPVWWKoAzg' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50,50,50,50]}
+        options={''}
+        onChange={''}
+        onClick={''}
+      >
+
+      </GoogleMapReact>
+    </div>
+
+  );
+}
+
+export default Map;
